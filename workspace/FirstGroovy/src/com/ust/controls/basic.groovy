3452e161bdb2024c648d19ea101535a397ff2599
puneet <puneetvashsiht@gmail.com>
2017-10-07 16:20:24 +0530

    regex and controls

---
+++ workspace/FirstGroovy/src/com/ust/controls/basic.groovy
@@ -0,0 +1,9 @@
+switch (6) {
+    case 0: println '0' ; break 
+    case [8,9,11]: println('8,9,11'); break 
+    case Double: println('floating point'); break
+    case {it % 3 == 0} : println ('Divisible by 3'); break
+    case 0..9: println('Range'); break
+    default: println 'default'
+
+}
---
+++ workspace/FirstGroovy/src/com/ust/regex/RegularExp.java
@@ -0,0 +1,5 @@
+package com.ust.regex;
+
+public class RegularExp {
+
+}
