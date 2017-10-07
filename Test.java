1cc2461454d9c9cc29c71aa65899dc1c7a222672
puneet <puneetvashsiht@gmail.com>
2017-10-07 09:52:44 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test{
+
+    public static void main(String[] args) {
+        int x = "2343";
+
+        System.out.println("test Java" + x);
+    }
+   
+}
+
---
+++ workspace/FirstGroovy/src/com/ust/TestGroovy.groovy
@@ -0,0 +1,3 @@
+package com.ust
+
+println 'Hello Groovy!!'
